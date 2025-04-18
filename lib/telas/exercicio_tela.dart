37a43aebab13b39a35987459a87b0e1999a91c34
Moisés Jorge <moisesjorge30@gmail.com>
2025-04-18 10:05:31 +0100

    adicionando os valores das models na tela e, substituindo a Column mais externa com um ListView para adicionar o comportamento de rolagem

--- lib/telas/exercicio_tela.dart
+++ lib/telas/exercicio_tela.dart
@@ -28,6 +28,306 @@ class ExercicioTela extends StatelessWidget {
       sentindo: "Ja senti alguma ativacao",
       data: "2025-04-19",
     ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Pouca ativacao hoje",
+      data: "2025-04-18",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "Ja senti alguma ativacao",
+      data: "2025-04-19",
+    ),
   ];
 
   @override
@@ -35,7 +335,7 @@ class ExercicioTela extends StatelessWidget {
     // Quando queremos criar "TELAS", o ideal eh que se use o widget "Scaffold": widget que facilita a criacao de telas no flutter
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Puxada Alta Pronada - Treino A"),
+        title: Text("${exercicioModelo.nome} - ${exercicioModelo.treino}"),
         backgroundColor: Colors.blue[700],
       ),
       floatingActionButton: FloatingActionButton(
@@ -46,23 +346,26 @@ class ExercicioTela extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView(
           children: [
             ElevatedButton(onPressed: () {}, child: const Text("Enviar foto")),
             const Text(
               "Como fazer?",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
-            Text(
-              "Segura com as duas maos na barra, mantem a coluna reta, e puxe",
-            ),
+            Text(exercicioModelo.comoFazer),
             Divider(),
             const Text(
               "Como estou me sentindo",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
             ),
-            const Text("Senti bastante ativacao hoje!"),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: List.generate(listaSentimentos.length, (index) {
+                SentimentoModelo sentimentoAgora = listaSentimentos[index];
+                return Text(sentimentoAgora.sentindo);
+              }),
+            ),
           ],
         ),
       ),
